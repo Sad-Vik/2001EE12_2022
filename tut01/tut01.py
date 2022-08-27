813780f9842513d06e6a37aa9dc9c332b4a897b3
Sadvik <68360117+Sad-Vik@users.noreply.github.com>
2022-08-27 13:14:17 +0530

    Answer

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,12 @@
-def factorial(int x):
-    
+def factorial(x):
+    if x == 0:
+        return 1
+    elif x > 0:
+        return x*factorial(x-1)
+    else:
+        return 'Enter a Valid Number!'
 
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+
+x = int(input("Enter the number whose factorial is to be found "))
+Result = factorial(x)
+print(Result)
