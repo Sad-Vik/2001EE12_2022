e0bdde2a675dc8b857ab11dd187a2bbf34f4ab0b
Sadvik <68360117+Sad-Vik@users.noreply.github.com>
2022-08-27 15:04:20 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,12 +1,13 @@
+# Defining Function
 def factorial(x):
     if x == 0:
         return 1
     elif x > 0:
-        return x*factorial(x-1)
+        return x*factorial(x-1)  # Recursion
     else:
         return 'Enter a Valid Number!'
 
 
 x = int(input("Enter the number whose factorial is to be found "))
-Result = factorial(x)
+Result = factorial(x)  # Calling the function
 print(Result)
