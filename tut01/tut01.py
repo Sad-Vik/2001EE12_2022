6cbdb623c1bb9955db49b32cc9b3ce4b4347956c
Sadvik <68360117+Sad-Vik@users.noreply.github.com>
2022-09-14 23:20:32 +0530

    answer

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,42 +1,20 @@
-'''
-def octact_identification(mod=5000):
-  ###Code
+#def octact_identification(mod):
 
-  mod = 5000
-  octact_identification(mod)
-'''
 import pandas as pd
-import math as math
+mod = 5000
+
+# Read the input file
 ip = pd.read_csv("octant_input.csv")
+# Creating a coloumn containg average of U,V,W
 ip.loc[0, "U_avg"] = ip["U"].mean()
 ip.loc[0, "V_avg"] = ip["V"].mean()
 ip.loc[0, "W_avg"] = ip["W"].mean()
+# Creating a coloumn with new U,V,W values
 ip["U1"] = ip["U"] - ip["U"].mean()
 ip["V1"] = ip["V"] - ip["V"].mean()
 ip["W1"] = ip["W"] - ip["W"].mean()
-
-'''
-for x, y, z in ip["U1"], ip["V1"], ip["W1"]:
-    if x > 0 and y > 0:
-        octant = 1
-        if z < 0:
-            octant = -1 * octant
-    elif x < 0 and y > 0:
-        octant = 2
-        if z < 0:
-            octant = -1 * octant
-    elif x < 0 and y < 0:
-        octant = 3
-        if z < 0:
-            octant = -1 * octant
-    elif x > 0 and y < 0:
-        octant = 4
-        if z < 0:
-            octant = -1 * octant
-    octant.append(ip["Octant"])
-
-'''
-mod = 5000
+# Creating a coloumn with Octant using loc which checks the condition
+# and assigns the value on right of = to it
 ip.loc[((ip.U1 > 0) & (ip.V1 > 0) & (ip.W1 > 0)), "Octant"] = "+1"
 ip.loc[((ip.U1 > 0) & (ip.V1 > 0) & (ip.W1 < 0)), "Octant"] = "-1"
 ip.loc[((ip.U1 < 0) & (ip.V1 > 0) & (ip.W1 > 0)), "Octant"] = "+2"
@@ -45,42 +23,67 @@ ip.loc[((ip.U1 < 0) & (ip.V1 < 0) & (ip.W1 > 0)), "Octant"] = "+3"
 ip.loc[((ip.U1 < 0) & (ip.V1 < 0) & (ip.W1 < 0)), "Octant"] = "-3"
 ip.loc[((ip.U1 > 0) & (ip.V1 < 0) & (ip.W1 > 0)), "Octant"] = "+4"
 ip.loc[((ip.U1 > 0) & (ip.V1 < 0) & (ip.W1 < 0)), "Octant"] = "-4"
+
 ip.loc[1, " "] = "User Input"
 ip.loc[[0, 1], "OctantID", ] = ["Overall Count", "Mod ="+str(mod)]
-'''
-for A in [1, -1, 2, -2, 3, -3, 4, -4]:
-    ip.loc[0, A] = len(ip.Octant == A)
-'''
-ip.loc[0, "+1"] = len(ip.Octant == +1)
-ip.loc[0, "-1"] = len(ip.Octant == -1)
-ip.loc[0, "+2"] = len(ip.Octant == +2)
-ip.loc[0, "-2"] = len(ip.Octant == -2)
-ip.loc[0, "+3"] = len(ip.Octant == +3)
-ip.loc[0, "-3"] = len(ip.Octant == -3)
-ip.loc[0, "+4"] = len(ip.Octant == +4)
-ip.loc[0, "-4"] = len(ip.Octant == -4)
-'''
-ip.loc[0, ["+1", "-1", "+2", "-2", "+3", "-3", "+4", "-4"]] = [len(ip.Octant == +1), len(ip.Octant == -1), len(
-    ip.Octant == +2), len(ip.Octant == -2), len(ip.Octant == +3), len(ip.Octant == -3), len(ip.Octant == +4), len(ip.Octant == -4)]
-ip.loc[i+2,"OctantID"]
-x = ip['Octant'].value_counts()
-print(x)
-y = ip['Octant'].value_counts(bins=5000)
-'''
-value = math.ceil((len(ip.Octant)/mod))
-for i in range(value):
-    if mod*(i+1) < len(ip.Octant):
-        ip.OctantID[i+2] = str(mod*i)+"-"+str(mod*(i+1) - 1)
-    else:
-        ip.OctantID[i+2] = str(mod*i)+"-"+str(len(ip.Octant)-1)
-#    for B in [1, -1, 2, -2, 3, -3, 4, -4]:
-#        ip.loc[i+2, B] = len(ip.Octant == B)
-    ip.loc[i+2, "+1"] = len(ip.Octant == +1)
-    ip.loc[i+2, "-1"] = len(ip.Octant == -1)
-    ip.loc[i+2, "+2"] = len(ip.Octant == +2)
-    ip.loc[i+2, "-2"] = len(ip.Octant == -2)
-    ip.loc[i+2, "+3"] = len(ip.Octant == +3)
-    ip.loc[i+2, "-3"] = len(ip.Octant == -3)
-    ip.loc[i+2, "+4"] = len(ip.Octant == +4)
-    ip.loc[i+2, "-4"] = len(ip.Octant == -4)
-ip.to_csv("octant_output1.csv", index=False)
+
+num = ip['Octant'].value_counts()
+
+# assigning count numbers from value count under all octants(+1,-1,+2,-2,+3,-3,+4,-4)
+ip.loc[0, "+1"] = num["+1"]
+ip.loc[0, "-1"] = num["-1"]
+ip.loc[0, "+2"] = num["+2"]
+ip.loc[0, "-2"] = num["-2"]
+ip.loc[0, "+3"] = num["+3"]
+ip.loc[0, "-3"] = num["-3"]
+ip.loc[0, "+4"] = num["+4"]
+ip.loc[0, "-4"] = num["-4"]
+# Assign a variable
+low = 0000
+m = mod
+i = 1
+while m <= len(ip):
+    ip['OctantID'][i+1] = str(low)+"-"+str(m-1)
+    p1 = 0
+    p2 = 0
+    p3 = 0
+    p4 = 0
+    n1 = 0
+    n2 = 0
+    n3 = 0
+    n4 = 0
+    for j in range(low, m):
+        if ip['Octant'][j] == "+1":
+            p1 = p1+1
+        elif ip['Octant'][j] == "-1":
+            n1 = n1+1
+        elif ip['Octant'][j] == "+2":
+            p2 = p2+1
+        elif ip['Octant'][j] == "+-2":
+            n2 = n2+1
+        elif ip['Octant'][j] == "+3":
+            p3 = p3+1
+        elif ip['Octant'][j] == "-3":
+            n3 = n3+1
+        elif ip['Octant'][j] == "+4":
+            p4 = p4+1
+        elif ip['Octant'][j] == "-4":
+            n4 = n4+1
+        ip.loc[i+2, "+1"] = p1
+        ip.loc[i+2, "-1"] = n1
+        ip.loc[i+2, "+2"] = p2
+        ip.loc[i+2, "-2"] = n2
+        ip.loc[i+2, "+3"] = p3
+        ip.loc[i+2, "-3"] = n3
+        ip.loc[i+2, "+4"] = p4
+        ip.loc[i+2, "-4"] = n4
+        low = m
+        i = i+1
+        m = mod*i
+        if m > len(ip):
+            m = len(ip)
+ip.to_csv("octant_output1.csv")
+
+
+mod = 5000
+#octact_identification(mod)
